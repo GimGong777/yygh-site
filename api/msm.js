933e6e5b05d0a04b38b641095609231cd14b04d9
gimgong <2368117490@qq.com>
2021-09-20 15:36:17 +0800

    预约挂号平台前台

---
+++ api/msm.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+const api_name = `/api/msm`
+
+export default {
+  // 发送验证码
+  sendCode(mobile) {
+    return request({
+      url: `${api_name}/send/${mobile}`,
+      method: 'get'
+    }) 
+  }
+}
